7d5bf44dad34bc0bd12fc255d0033a76256b6a4c
Alex Huang <122187419+nepallium@users.noreply.github.com>
2025-01-26 13:06:58 -0500

    Remove decimal point if no decimals & Format code

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let currentNum = null;
 let isFloat = false;
 let firstNumAsString;
 
-buttons.forEach(button => 
+buttons.forEach(button =>
     button.addEventListener("click", (event) => click(event.target)) // pass the element (button)
 );
 
@@ -17,7 +17,7 @@ buttons.forEach(button =>
 document.addEventListener("keydown", (event) => {
     let keypress = event.key;
     let fakeClickBtn;
-    
+
     if (Number.isInteger(+keypress)) {
         // Simulate getting the event.target of a click
         fakeClickBtn = document.createElement("button");
@@ -36,7 +36,7 @@ document.addEventListener("keydown", (event) => {
                 break;
             case "=":
             case "+":
-            case "x":   
+            case "x":
             case "/":
             case "-":
             case ".":
@@ -47,33 +47,35 @@ document.addEventListener("keydown", (event) => {
             default: null;
         }
     }
-    
+
 });
 
 function click(clickedButton) {
     let btnContent = clickedButton.textContent;
     switch (clickedButton.className) {
         case "": // user clicks on number
+            // Remove filler text (used for keeping calcLog above entry)
             if (entry.textContent === "filler") {
                 entry.textContent = "";
                 entry.style.color = "#f5f9fd";
             }
+
             // CurrentNum depends on itself only, not on the screen output!
             if (isFloat) {
                 let [tmp, numOfDecimals] = (entry.textContent.split("."));
-                currentNum = currentNum + (+btnContent)/Math.pow(10, numOfDecimals.length + 1);
+                currentNum = currentNum + (+btnContent) / Math.pow(10, numOfDecimals.length + 1);
                 entry.textContent += btnContent; // using string notation since JS bugs out with large decimals
             }
             else {
-                currentNum = currentNum*10 + +btnContent;
+                currentNum = currentNum * 10 + +btnContent;
                 entry.textContent = currentNum;
             }
-            
+
             // If array length == 2, it means there is an operator inside the array.
             // Therefore, no need to update value at inputsArray[0]
             let indexToChange = inputsArray.length >= 2 ? 2 : 0;
             inputsArray[indexToChange] = currentNum;
-            
+
             // Clears log if a NEW operation is started. 
             // (There is an operation already present on the log, and user is entering a NEW number
             // therefore, wants to erase previous operation from memory)
@@ -84,50 +86,54 @@ function click(clickedButton) {
             break;
 
         case "operator":
-            if (typeof inputsArray[0] === "number" 
+            if (typeof inputsArray[0] === "number"
                 && (inputsArray.length === 1 ||
                     inputsArray.length === 2) // If user wants to change the operator
-                ) {
+            ) {
+                if (isFloat && entry.textContent.at(-1) === ".") {
+                    entry.textContent = entry.textContent.slice(0, -1);
+                }
+
                 // Reset CurrentNum to allow user to input next number for the operation
                 currentNum = null;
-                
+
                 // IF not enough elements to perform an operation on: Break out of switch
                 if (inputsArray.length < 3) {
                     // If user wants to change the operator
                     if (inputsArray.length === 2) {
                         inputsArray.pop();
                     }
-                    inputsArray.push(btnContent)
+                    inputsArray.push(btnContent) // add operator to array
                     isFloat = false; // reset isFloat so that when entering a new number, it doesn't add onto the decimals of the previous number
-                    
+
                     // CALCULATOR LOG
                     firstNumAsString = entry.textContent;
                     calcLog.textContent = `${firstNumAsString} ${inputsArray[1]}`;
-                    
+
                     break;
                 }
                 // ElSE perform the operation by using switch fallthrough (instead of breaking)
                 // aka perform case "equal" below
 
             }
-            
+
         case "equal":
-            if (inputsArray.length === 3){
+            if (inputsArray.length === 3) {
                 // Longer way to account for bad JS rounding for floats
                 calcLog.textContent = `${firstNumAsString} ${inputsArray[1]} ${entry.textContent} = `;
-                
+
                 let result = operate(...inputsArray);
                 entry.textContent = result;
-                
-                inputsArray = [result]; 
+
+                inputsArray = [result];
                 // Will not cause problem if user enters another number directly,
                 // since inputsArray[0] will be replaced by said number since array.length != 2 (check logic under case "")
-                
+
                 currentNum = null;
                 isFloat = false; // reset isFloat so that when entering a new number, it doesn't add onto the decimals of the previous number
-                
+
                 // Keep operator in memory if an operator was clicked before pressing "="
-                if (btnContent !== "=" && 
+                if (btnContent !== "=" &&
                     (!(inputsArray.includes("⌐(ಠ۾ಠ)¬")))
                 ) {
                     inputsArray.push(btnContent);
@@ -136,7 +142,7 @@ function click(clickedButton) {
                 }
             }
             break;
-        
+
         case "clear":
             inputsArray = [];
             entry.textContent = "";
@@ -156,17 +162,18 @@ function click(clickedButton) {
             // Delete digit if currentNum holds a value
             else if (currentNum !== null || entry.textContent === "0") {
                 entry.textContent = entry.textContent.slice(0, -1);
-                
+
                 // IF the currentNum is 0, this means user has deleted the whole number (except case: 0.xxx)
                 // Remove it from the array
                 if (entry.textContent === "") {
+                    // Keep the calcLog text ABOVE the entry box
                     if (calcLog.textContent !== "") {
                         entry.textContent = "filler";
                         entry.style.color = "#7CBDD5" // screen blue
                     }
                     currentNum = null;
                     inputsArray.pop();
-                } 
+                }
                 // ELSE update the currentNum
                 else {
                     currentNum = +entry.textContent;
@@ -179,7 +186,15 @@ function click(clickedButton) {
         case "dot":
             if (isFloat === false) {
                 if (currentNum === null) { // user pressed dot without entering a number first
+                    currentNum = 0;
+                    let indexToChange = inputsArray.length >= 2 ? 2 : 0;
+                    inputsArray[indexToChange] = currentNum;
                     entry.textContent = "0"
+
+                    // Clear calcLog if "dot" was pressed after an operation already finished calculatting
+                    if (calcLog.textContent.includes("=")) {
+                        calcLog.textContent = "";
+                    }
                 }
                 entry.textContent += ".";
                 isFloat = true;
@@ -201,19 +216,19 @@ function operate(num1, operator, num2) {
 
 
 function add(a, b) {
-    return a+b;
+    return a + b;
 }
-function subtract(a,b) {
-    return a-b;
+function subtract(a, b) {
+    return a - b;
 }
-function multiply(a,b) {
-    return a*b;
+function multiply(a, b) {
+    return a * b;
 }
-function divide(a,b) {
+function divide(a, b) {
     if (b === 0) {
         return "⌐(ಠ۾ಠ)¬";
     }
-    return +(a/b).toFixed(4);
+    return +(a / b).toFixed(4);
 }
 // Darken button when hovering
 buttons.forEach(button =>
